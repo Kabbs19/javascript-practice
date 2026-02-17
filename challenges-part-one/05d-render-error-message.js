46780399cfd90ec1d281616dadd491da00e0d43f
Kabbs19 <77408435+Kabbs19@users.noreply.github.com>
2026-02-17 22:46:49 +0000

    Worked on 05d, JS done, html in place - next work on css

--- challenges-part-one/05d-render-error-message.js
+++ challenges-part-one/05d-render-error-message.js
@@ -1,3 +1,13 @@
 // When the user clicks the purchase button, render out
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
+
+let FirstName = "Eric"
+const purchaseBtn = document.getElementById("purchase-btn")
+const purchaseBtnError = document.getElementById("purchase-btn-error")
+
+purchaseBtn.addEventListener("click", errorMessage)
+
+function errorMessage() {
+    purchaseBtnError.innerHTML = `Sorry ${FirstName},  something went wrong. Please try again.`
+}
