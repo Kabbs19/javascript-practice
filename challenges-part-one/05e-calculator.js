0c91cc99a6cb0dd50f91eee6830c38c35a92bb4f
Kabbs19 <77408435+Kabbs19@users.noreply.github.com>
2026-02-21 11:47:16 +0000

    Add add() function to add-btn id

--- challenges-part-one/05e-calculator.js
+++ challenges-part-one/05e-calculator.js
@@ -1,9 +1,13 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+let num1 = document.getElementById("num1-el")
+let num2 = document.getElementById("num2-el")
+
+const addNum = document.getElementById("add-btn")
+const subtractNum = document.getElementById("subtract-btn")
+const divideNum = document.getElementById("divide-btn")
+const multiplyNum = document.getElementById("multiply-btn")
 
 let sum = document.getElementById("sum-el")
+
 
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
@@ -12,3 +16,11 @@ let sum = document.getElementById("sum-el")
 
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+addNum.addEventListener("click", add)
+    
+function add() {
+    const firstNum = Number(num1.value)
+    const secondNum = Number(num2.value)
+    sum.textContent = firstNum + secondNum
+}
